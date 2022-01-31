675c03128f3fa8f7f9157b110930fa40600ee088
Narresx <92748159+Narresx@users.noreply.github.com>
2022-01-31 11:02:19 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log('JS OK', Vue);
+
+Vue.config.devtools = true;
+
+const root = new Vue({
+
+    el: '#root',
+
+    data: {
+
+        user: {
+            name: 'Nome Utente',
+            avatar: '_io'
+        },
+
+        contacts: [
+            {
+                name: 'Michele',
+                avatar: '_1',
+            },
+            {
+                name: 'Fabio',
+                avatar: '_2',
+            },
+            {
+                name: 'Samuele',
+                avatar: '_3',
+            },
+            {
+                name: 'Luisa',
+                avatar: '_4',
+            },
+        ]
+    }
+
+});
